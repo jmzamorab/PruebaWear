ac20333c5e36b569f9bc5f07291fa55ecb808b03
jmzamorab <josem.zamorab@gmail.com>
2017-06-09 00:28:56 +0200

    Abrir teléfono desde el smartwatch

--- app/src/main/java/es/upv/master/pruebawear/MainActivity.java
+++ app/src/main/java/es/upv/master/pruebawear/MainActivity.java
@@ -1,13 +1,15 @@
 package es.upv.master.pruebawear;
 
 import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
-import android.support.v4.app.NotificationManagerCompat;
 
 //**********************************
 //adb -d forward tcp:5601 tcp:5601
@@ -22,16 +24,21 @@ public class MainActivity extends AppCompatActivity {
         wearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    int notificacionId = 001;
-                    Notification notificacion = new NotificationCompat.Builder(MainActivity.this)
-                            .setSmallIcon(R.mipmap.ic_launcher)
-                            .setContentTitle("Título")
-                            //.setContentText("Notificación Android Wear")
-                            .setContentText(Html.fromHtml("<b>Notificación</b> <u>Android <i>Wear</i></u>"))
-                            .build();
-                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
-                    notificationManager.notify(notificacionId, notificacion);
-                }
+                // Creamos intención pendiente
+                Intent intencionMapa = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent intencionPendienteMapa = PendingIntent.getActivity(MainActivity.this, 0, intencionMapa, 0);
+
+                int notificacionId = 001;
+                Notification notificacion = new NotificationCompat.Builder(MainActivity.this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Título")
+                        //.setContentText("Notificación Android Wear")
+                        .setContentText(Html.fromHtml("<b>Notificación</b> <u>Android <i>Wear</i></u>"))
+                        .setContentIntent(intencionPendienteMapa)
+                        .build();
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
+                notificationManager.notify(notificacionId, notificacion);
+            }
         });
     }
 }
