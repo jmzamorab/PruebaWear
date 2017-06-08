87671e14b07ce793e1903dadc38320c2942f83e8
jmzamorab <josem.zamorab@gmail.com>
2017-06-09 00:45:06 +0200

    Abrir teléfono desde el smartwatch y se nos posiciona en la UPV con maps y/o abre tfno para llamar Con icono "chulo"

--- app/src/main/java/es/upv/master/pruebawear/MainActivity.java
+++ app/src/main/java/es/upv/master/pruebawear/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                         //.setContentText("Notificación Android Wear")
                         .setContentText(Html.fromHtml("<b>Notificación</b> <u>Android <i>Wear</i></u>"))
                         .setContentIntent(intencionPendienteMapa)
-                        .addAction(android.R.drawable.ic_menu_call, "llamar", intencionPendienteLlamar)
+                        .addAction(R.mipmap.ic_action_call, "llamar", intencionPendienteLlamar)
                         .build();
                 NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
                 notificationManager.notify(notificacionId, notificacion);
