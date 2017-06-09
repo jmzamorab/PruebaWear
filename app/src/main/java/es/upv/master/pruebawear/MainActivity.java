1e57bdf93abbd3fdffe33e1e45d0e0f698869ea8
jmzamorab <josem.zamorab@gmail.com>
2017-06-09 22:06:54 +0200

    Voy página 48

--- app/src/main/java/es/upv/master/pruebawear/MainActivity.java
+++ app/src/main/java/es/upv/master/pruebawear/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         wearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String s="Texto largo de prueba, .... en un lugar de la mancha, de cuyo nombre no quiero acordarme ....";
+                String s = "MI CARRROOOOOOO  Texto largo de prueba, .... en un lugar de la mancha, de cuyo nombre no quiero acordarme ....";
                 // Creamos intención pendiente
                 //Intent intencionMapa = new Intent(MainActivity.this, MainActivity.class);
                 Intent intencionMapa = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=universidad+politecnica+valencia"));
@@ -43,6 +43,11 @@ public class MainActivity extends AppCompatActivity {
                 listaAcciones.add(accion);
                 listaAcciones.add(new NotificationCompat.Action(R.mipmap.ic_action_locate, "Ver mapa", intencionPendienteMapa));
 
+                NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.WearableExtender().
+                        setHintHideIcon(true).
+                        setBackground(BitmapFactory.decodeResource(getResources(), R.drawable.escudo_upv))
+                        .addActions(listaAcciones);
+
                 Notification notificacion = new NotificationCompat.Builder(MainActivity.this)
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle("Título")
@@ -51,8 +56,9 @@ public class MainActivity extends AppCompatActivity {
                         // .setContentIntent(intencionPendienteMapa)
                         .addAction(R.mipmap.ic_action_call, "llamar", intencionPendienteLlamar)
                         .extend(new NotificationCompat.WearableExtender().addActions(listaAcciones))
-                        .setLargeIcon(BitmapFactory.decodeResource( getResources(), R.drawable.escudo_upv))
-                        .setStyle(new NotificationCompat.BigTextStyle().bigText(s+s))
+                       // .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.escudo_upv))
+                        .extend(wearableExtender)
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(s + s))
                         .build();
                 NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
                 notificationManager.notify(notificacionId, notificacion);
