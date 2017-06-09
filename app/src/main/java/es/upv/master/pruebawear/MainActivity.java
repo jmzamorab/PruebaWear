debc60211701f7393b52453e44477779d5b843a6
jmzamorab <josem.zamorab@gmail.com>
2017-06-09 22:13:23 +0200

    Añadido una 2ª pagina. Voy pg 49, tengo que hacer Añadir 3ª página Adicional

--- app/src/main/java/es/upv/master/pruebawear/MainActivity.java
+++ app/src/main/java/es/upv/master/pruebawear/MainActivity.java
@@ -43,10 +43,17 @@ public class MainActivity extends AppCompatActivity {
                 listaAcciones.add(accion);
                 listaAcciones.add(new NotificationCompat.Action(R.mipmap.ic_action_locate, "Ver mapa", intencionPendienteMapa));
 
+                NotificationCompat.BigTextStyle segundaPg = new NotificationCompat.BigTextStyle();
+                segundaPg.setBigContentTitle("Página 2").bigText("Más texto.");
+                // Creamos una notification para la segunda página Notification
+                Notification notificacionPg2 = new NotificationCompat.Builder( MainActivity.this) .setStyle(segundaPg) .build();
+
+
                 NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.WearableExtender().
                         setHintHideIcon(true).
                         setBackground(BitmapFactory.decodeResource(getResources(), R.drawable.escudo_upv))
-                        .addActions(listaAcciones);
+                        .addActions(listaAcciones).
+                        addPage(notificacionPg2);
 
                 Notification notificacion = new NotificationCompat.Builder(MainActivity.this)
                         .setSmallIcon(R.mipmap.ic_launcher)
@@ -56,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
                         // .setContentIntent(intencionPendienteMapa)
                         .addAction(R.mipmap.ic_action_call, "llamar", intencionPendienteLlamar)
                         .extend(new NotificationCompat.WearableExtender().addActions(listaAcciones))
-                       // .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.escudo_upv))
+                        // .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.escudo_upv))
                         .extend(wearableExtender)
                         .setStyle(new NotificationCompat.BigTextStyle().bigText(s + s))
                         .build();
